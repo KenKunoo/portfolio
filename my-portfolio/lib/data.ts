2cec2cb207e08d7918d7b65a029712e6d2e872f3
Kenny Fong <kefong@ucsd.edu>
2023-12-19 19:03:56 -0800

    finished pictures and projects

--- my-portfolio/lib/data.ts
+++ my-portfolio/lib/data.ts
@@ -3,11 +3,11 @@ import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
 
-import corpcommentImg from "@/public/corpcomment.png";
+import proj110 from "@/public/project110.png";
 import carSafetyImg from "@/public/carsafety.png";
 import gameImg from "@/public/game.png";
 import websitePic from "@/public/websitepic.png";
-
+import cryptoPic from "@/public/crypto.png";
 
 export const links = [
   {
@@ -33,39 +33,16 @@ export const links = [
 ] as const;
 
 export const experiencesData = [
-  {
-    title: "Graduated bootcamp",
-    location: "Miami, FL",
-    description:
-      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2019",
-  },
-  {
-    title: "Front-End Developer",
-    location: "Orlando, FL",
-    description:
-      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
-    icon: React.createElement(CgWorkAlt),
-    date: "2019 - 2021",
-  },
-  {
-    title: "Full-Stack Developer",
-    location: "Houston, TX",
-    description:
-      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
-    icon: React.createElement(FaReact),
-    date: "2021 - present",
-  },
+  
 ] as const;
 
 export const projectsData = [
   {
-    title: "Note Taking Application",
+    title: "Notetaking App",
     description:
       "Software project that allowed students and professor to communicate with intuitive feedback.",
     tags: ["HTML", "CSS", "Javascript", "API", "Agile"],
-    imageUrl: corpcommentImg,
+    imageUrl: proj110,
     projectLink: 'https://github.com/KenKunoo/Modern-Car-Safety-Features',
   },
   {
@@ -77,11 +54,11 @@ export const projectsData = [
     projectLink: 'https://github.com/KenKunoo/Modern-Car-Safety-Features',
   },
   {
-    title: "Modern Car Safety",
+    title: "Cypto Screener App",
     description:
-      "Data Science project with five other people, comparing modern vehicle safety to older vehicles.",
-    tags: ["PANDAS", "Python", "Jupyter", "Seaborn"],
-    imageUrl: carSafetyImg,
+      "Track cryptocurrency through real-time data and organize trade with a simple interface",
+    tags: ["React", "Tailwind", "Context API", "Javascript"],
+    imageUrl: cryptoPic,
     projectLink: 'https://github.com/KenKunoo/Modern-Car-Safety-Features',
   },
   {
