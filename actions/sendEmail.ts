f6da839cb98f8d80d95d68faf72965109a33d0a7
KenKunoo <kennyfong12@yahoo.com>
2023-12-27 03:03:55 -0800

    contact

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -12,7 +12,7 @@ export const sendEmail = async (formData: FormData) => {
   const message = formData.get("message");
 
   // simple server-side validation
-  if (!validateString(senderEmail, 500)) {
+  if (!validateString(senderEmail, 150)) {
     return {
       error: "Invalid sender email",
     };
@@ -27,7 +27,7 @@ export const sendEmail = async (formData: FormData) => {
   try {
     data = await resend.emails.send({
       from: "Contact Form <onboarding@resend.dev>",
-      to: "bytegrad@gmail.com",
+      to: "kennyfong12@yahoo.com",
       subject: "Message from contact form",
       reply_to: senderEmail,
       react: React.createElement(ContactFormEmail, {
--- components/contact.tsx
+++ components/contact.tsx
@@ -23,14 +23,10 @@ export default function Contact() {
             }}>
             <SectionHeading> <span className="font-bold"> Contact Me </span> </SectionHeading>
 
-            <form className="mt-10 flex flex-col"
-                    action={async(formData : FormData) => {
-                        "use server";
-                        console.log(formData);
-                        await sendEmail(formData);
-                    }}>
-                <input className = "h-12 rounded-lg borderBlack p-4" type="email" name="senderEmail" required maxLength = {100} placeholder = "Your email"/>
-                <textarea className = "h-52 my-3 rounded-lg borderBlack p-4" name = "nessage" required maxLength = {500} placeholder = "Your message"/>
+            <form className="mt-10 flex flex-col" action={async (formData) => {await sendEmail(formData)}}>
+                    
+                <input className = "h-12 rounded-lg borderBlack p-4" type="email" name="senderEmail" required maxLength = {150} placeholder = "Your email"/>
+                <textarea className = "h-52 my-3 rounded-lg borderBlack p-4" name = "nessage" required maxLength = {5000} placeholder = "Your message"/>
                 <button type="submit" className="group flex items-center justify-center h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-90 hover:scale-90 hover:bg-gray-950 active:scale-100">
                     Submit <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:translate-y-0"/>{""}
                 </button>
